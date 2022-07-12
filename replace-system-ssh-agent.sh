2b2a24f212a837ff4dd92ce8e58d8cc4dea3432b
Benjamin Goodacre <f1@goodacre.name>
2022-07-12 11:43:37 +0800

    Add replace-system-ssh-agent.sh
    * Allows the system ssh-agent to be replaced on a system where you are not root,
      or without breaking SIP on Mac OSX.
    * Useful if you want to use FIDO / yubikey ssh which requires homebrew openssh

---
+++ replace-system-ssh-agent.sh
@@ -0,0 +1,32 @@
+# Replace system ssh-agent
+# This is typically used to replace the Mac OSX ssh-agent with the newer, FIDO aware, homebrew-installed openssh ssh-agent
+# but it can be used to replace the ssh-agent on any system where you do not have root privs.
+#
+# You should also ensure the other openssh binaries are in a high-precedence in your $PATH
+
+NEW_SSH_AGENT=${1-$(brew --prefix openssh)/bin/ssh-agent}
+
+SSH_ENV=${HOME}/.ssh/environment
+
+function start_agent {
+     echo -n "Initialising new SSH agent with ${NEW_SSH_AGENT}..."
+     ${NEW_SSH_AGENT} | sed 's/^echo/#echo/' > ${SSH_ENV}
+     echo succeeded
+     touch ${SSH_ENV}
+     chmod 600 ${SSH_ENV}
+     . ${SSH_ENV} > /dev/null
+  #   /usr/bin/ssh-add;
+}
+
+# Source SSH settings, if applicable
+
+if [ -f "${SSH_ENV}" ]; then
+     . ${SSH_ENV} > /dev/null
+     #ps ${SSH_AGENT_PID} not used as it does not work in cygwin
+     ps -ef | grep ${SSH_AGENT_PID} | grep ssh-agent$ > /dev/null || {
+         start_agent;
+     }
+else
+     start_agent;
+fi
+
