8d6c5391764d0fbb588e65a35d814352f2cb66d3
Benjamin Goodacre <f1@goodacre.name>
2024-02-27 11:24:50 +0000

    Add kubernetes context choice
    * Allow the node choser to have some more generic names
    * setup hgg-choice to use above
    * kubernetes-context-choser
    * Add menu_title as var

--- node-choser/index.js
+++ node-choser/index.js
@@ -2,11 +2,13 @@ import inquirer from 'inquirer';
 import readline from 'readline';
 import fs from 'fs';
 
-var filename = process.env.hgg_file;
+var filename = process.env.menu;
 
 var lines = fs.readFileSync(filename, 'utf-8')
     .split('\n')
     .filter(Boolean);
+
+var menutitle = process.env.menu_title;
 
 inquirer
   .prompt([
@@ -14,12 +16,12 @@ inquirer
       type: 'list',
       pageSize: 30,
       name: 'chosenCommand',
-      message: 'history-global-grep choose command:',
+      message: menutitle,
       choices: lines
     },
   ])
   .then(answers => {
-    fs.writeFile(process.env.hgg_chosen, answers.chosenCommand, err => {
+    fs.writeFile(process.env.menu_chosen, answers.chosenCommand, err => {
       if (err) {
         console.error(err);
       }
